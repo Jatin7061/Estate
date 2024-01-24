7daa3c57035cd478f8284cfb359600fb11b41296
Jatin <jatins7061@gmail.com>
2024-01-24 22:22:54 +0530

    Create Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import About from './Pages/About'
 import Signin from './Pages/Signin'
 import Signout from './Pages/Signout'
 import Profile from './Pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
