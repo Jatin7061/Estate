7200fa3ac261d6b0cb524f8dec14314b89285938
Jatin <jatins7061@gmail.com>
2024-02-09 23:03:14 +0530

    Complete delete Listing functionality

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -119,6 +119,22 @@ const Profile = () => {
       setshowListingError(true)
     }
    }
+
+   const handleListingDelete =async(listingId)=>{
+   try {
+    const res = await fetch(`/api/listing/delete/${listingId}`,{
+      method: "DELETE"
+    })
+    const data = await res.json();
+    if(data.success === false){
+      console.log(data.message)
+      return
+    }
+    setUserListing((prev)=> prev.filter((listing)=>listing._id !== listingId)) 
+   } catch (error) {
+    next(error)
+   }
+   }
    console.log(formData)
   return (
     <div className='p-3 max-w-lg mx-auto '>
@@ -167,7 +183,7 @@ const Profile = () => {
      <p>{listing.name}</p>
     </Link>
     <div className='flex flex-col'>
-      <button className='text-red-700'>Delete</button>
+      <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700'>Delete</button>
       <button className='text-green-700'>Edit</button>
 
     </div>
--- server/routers/listingrouter.js
+++ server/routers/listingrouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createListing } from '../controllers/listingcontroller.js';
+import { createListing, deleteListing } from '../controllers/listingcontroller.js';
 import { verifyToken } from '../utils/verfiyUser.js';
 const router = express.Router();
 
 router.post("/create",verifyToken,createListing);
+router.delete("/delete/:id",verifyToken,deleteListing)
 
 
 export default router;
