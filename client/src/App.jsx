a8ea47868c68fffb2ec5957f58eb7f3a49c05601
Jatin <jatins7061@gmail.com>
2024-02-12 23:39:10 +0530

    Complete Listing functinality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import SignUp from './Pages/SignUp'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './Pages/CreateListing'
 import UpdateListing from './Pages/UpdateListing'
+import Listing from './Pages/Listing'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
       <Route path='/about' element={<About/>}/>
       <Route path='/sign-in' element={<Signin/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/listing/:listingId' element={<Listing/>}/>
       <Route element={<PrivateRoute/>}>
       <Route path='/profile' element={<Profile/>}/>
       <Route path='/create-listing' element={<CreateListing/>}/>
