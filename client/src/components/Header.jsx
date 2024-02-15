58e5ef745f4916c06d87f048e2acd0fd4aa56bf2
Jatin <jatins7061@gmail.com>
2024-02-15 12:27:17 +0530

    Header Search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 import {  useSelector } from 'react-redux'
 
 const Header = () => {
+  const [search,setsearch] = useState('')
   const {currentUser} = useSelector((state)=>state.user)
+  const navigate = useNavigate();
+
+  const handleSubmit =(e)=>{
+  e.preventDefault();
+  const urlParams = new URLSearchParams(window.location.search);
+  urlParams.set('search',search);
+  const searchQuery = urlParams.toString();
+  navigate(`/search?${searchQuery}`)
+  }
+
+
+  useEffect(()=>{
+  const urlParams = new URLSearchParams(location.search);
+  const searchTermFormUrl = urlParams.get('search');
+  if(searchTermFormUrl){
+    setsearch(searchTermFormUrl)
+  }
+  },[location.search])
+
   return (
 <header className='bg-slate-300 shadow:md'>
     <div className='flex justify-between items-center max-w-6xl mx-auto p-2'>
@@ -14,9 +34,11 @@ const Header = () => {
     <span className='text-slate-900'>Estate</span>
     </h1>
     </Link>
-    <form className='flex items-center bg-slate-100 p-2 rounded-lg'>
-        <input type='text' placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-60'/>
+    <form onSubmit={handleSubmit} className='flex items-center bg-slate-100 p-2 rounded-lg'>
+        <input type='text' value={search} onChange={(e)=>setsearch(e.target.value)} placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-60'/>
+        <button >
         <FaSearch className='text-slate-700'/>
+        </button>
     </form>
     <ul className='flex gap-4'>
         <Link to='/'>
